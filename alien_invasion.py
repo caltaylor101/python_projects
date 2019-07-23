1d28bfe140b113d0de23697b2c4bef100e4be15f
caltaylor101 <34333250+caltaylor101@users.noreply.github.com>
2019-07-22 18:14:54 -0700

    1 row of aliens

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,10 +1,14 @@
 import sys
 import pygame
+from pygame.sprite import Group
+from alien import Alien
 
 
 from settings import Settings
 from ship import Ship
+
 import game_functions as gf
+
 
 def run_game():
     #Initialize game, settings, and create a screen object.
@@ -12,22 +16,34 @@ def run_game():
     ai_settings = Settings()
     screen = pygame.display.set_mode(
         (ai_settings.screen_width, ai_settings.screen_height))
-    screen = pygame.display.set_mode((1000,600))
+    
     pygame.display.set_caption("Alien Invasion")
 
-    #Make a ship.
+    #Make a ship. A group of bullets, and a group of aliens.
     ship = Ship(ai_settings, screen)
-    
+    #Make a group to store bullets
+    bullets = Group()
+    aliens = Group()
 
-    #Set background color.
-    bg_color = (230,230,230)
+    #Create fleet of aliens.
+    gf.create_fleet(ai_settings, screen, aliens)
+    
+    #Make an alien
+    alien = Alien(ai_settings, screen)
+    
 
     #Main loop for game
     while True:
+        gf.check_events(ai_settings, screen, ship, bullets)
+        
+        
         #Watch for keyboard and mouse events.
-        gf.check_events(ship)
+        
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets)
+        
+        
 
         
 run_game()
